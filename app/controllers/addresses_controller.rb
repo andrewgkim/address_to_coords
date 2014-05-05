7d31edac36ed6ea00eee2a00186cc372b3a52f22
andrewgkim <andrewgkim@gmail.com>
2014-05-05 01:40:55 -0500

    Fixed so address is dynamic

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -3,11 +3,11 @@ require 'json'
 
 class AddressesController < ApplicationController
   def fetch_coordinates
-    @address = "the corner of Foster and Sheridan"
+    @address = "the corner of foster and sheridan"
     @url_safe_address = URI.encode(@address)
 
     # Your code goes here.
-    url = "http://maps.googleapis.com/maps/api/geocode/json?address=the+corner+of+Foster+and+Sheridan&sensor=false"
+    url = "http://maps.googleapis.com/maps/api/geocode/json?address=#{@url_safe_address}&sensor=true"
     raw_data = open(url).read
     parsed_data = JSON.parse(raw_data)
     @latitude = parsed_data["results"][0]["geometry"]["location"]["lat"]
